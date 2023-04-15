8fb624537068df5c86dd909c191f072a9ed330bd
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-15 22:24:45 +0900

    test: findFirst().orElse() 메서드를 활용한 특정 조건 필터링 구현

--- Lambda/src/PersonTest.java
+++ Lambda/src/PersonTest.java
@@ -1,5 +1,5 @@
-import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,9 +8,6 @@ import java.util.stream.Collectors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class PersonTest {
     private List<Person> people;
@@ -40,19 +37,21 @@ class PersonTest {
         assertEquals("dk", filteredPeople.get(0).getName());
     }
     
-    @DisplayName("모든 사람의 이름을 대문자로 변환하여 리스트로 반환하는지 확인")
-    @ParameterizedTest
-    @ValueSource(strings = {"BK", "DK", "YK", "UK"})
-    void 대문자_변환(String upperName) {
-        // Given: Person 클래스
-        
+    @Test
+    @DisplayName("특정 나이로 필터링이 되는지 확인:31세 이상이 최소 1명 이상")
+    void 특정_나이로_필터링_31세_이상() {
+        // Given
+        int ageToFilter = 31;
+
         // When
-        List<String> upperCaseNames = people.stream()
-                        .map(person -> person.getName().toUpperCase())
-                        .collect(Collectors.toList());
-        
+        String result = people.stream()
+                        .filter(person -> person.getAge() >= ageToFilter )
+                        .map(Person::getName)
+                        .findFirst()
+                        .orElse("없음");
+
         // Then
-        assertTrue(upperCaseNames.contains(upperName));
-        
+        assertThat(result).isEqualTo("없음");
     }
+
 }
