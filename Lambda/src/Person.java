d32e2bb471e5a507ddfaef6936d46c9e5f7eeb9b
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-19 16:36:12 +0900

    fix(Person): 50세 이하만 가입할 수 있는 요구사항 수정
    생성자를 통해 전달된 int형의 age 매개변수를 검사하는 로직을 추가했습니다.

--- Lambda/src/Person.java
+++ Lambda/src/Person.java
@@ -1,10 +1,18 @@
 public class Person {
+    private static final int MAX_AGE = 50;
     private String name;
     private int age;
     
     public Person( String name, int age ) {
+        if( isInvalidAge(age) ) {
+            throw new RuntimeException("50세 이하만 가입이 가능합니다.");
+        }
         this.name = name;
         this.age = age;
+    }
+
+    private boolean isInvalidAge(int age) {
+        return age > MAX_AGE;
     }
 
     public String getName() {
