9a7dae0587fff5da37110cb843009f02ce4d3bc0
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-06 22:35:47 +0900

    test: matches() 메서드와 find() 메서드 관련 테스트 코드 추가

--- unit-testing/src/Matcher/MatcherTest.java
+++ unit-testing/src/Matcher/MatcherTest.java
@@ -4,17 +4,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.awt.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class MatcherTest {
 
     @Test
     @DisplayName("패턴 클래스의 matches 메서드()를 통해서 정규식과 입력값을 비교")
-    void coordinate_custom_regex_test() {
+    void coordinate_custom_regex_Pattern_test() {
         // given
         String regex = "\\((\\d+),(\\d+)\\)";
 
@@ -25,14 +28,44 @@ public class MatcherTest {
         assertThat(actual).isTrue();
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"a(100,300)", "a(100,300)b", "a(100,300)bc", "a(100,300)sdfdsxcz" })
+    @DisplayName("정규식과 일부 입력 문자열만 동일하고, 전체 입력 문자열이 다를때 false를 반환하는지 확인")
+    void coordinate_custom_regex_Pattern_test2(String input) {
+        // given
+        String regex = "\\((\\d+),(\\d+)\\)";
+
+        //when
+        boolean actual = Pattern.matches(regex, input);
+
+        //then
+        assertThat(actual).isFalse();
+    }
+
+
     @Test
     @DisplayName("패턴 클래스의 compile() 메서드를 통해 최초 1회 Matcher 클래스를 만들고, 이후 입력값과 비교")
-    void coordinate_custom_regex_test2() {
+    void coordinate_custom_regex_matcher_test1() {
         // given
         Pattern pattern = Pattern.compile("\\((\\d+),(\\d+)\\)");
 
         //when
         Matcher matcher = pattern.matcher("(100,300)");
+
+        //then
+        assertThat(matcher.find()).isEqualTo(true);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"a(100,300)", "a(100,300)b", "a(100,300)bc", "a(100,300)sdfdsxcz" })
+    @DisplayName("정규식과 일부 입력 문자열만 동일하고, 전체 입력 문자열이 달라도 true를 반환하는지 확인")
+    void coordinate_custom_regex_matcher_test2(String input) {
+        // given
+        Pattern pattern = Pattern.compile("\\((\\d+),(\\d+)\\)");
+
+
+        //when
+        Matcher matcher = pattern.matcher(input);
 
         //then
         assertThat(matcher.find()).isEqualTo(true);
