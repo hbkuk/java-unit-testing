dd20b2379241013ad1fa0a8f20dc226cd256fb16
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-05-15 23:21:49 +0900

    feat: 빌더 패턴으로 객체를 생성 시 이를 테스트하는 코드 추가

---
+++ unit-testing/src/builder/BoardTest.java
@@ -0,0 +1,24 @@
+package builder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BoardTest {
+
+    @Test
+    void create_board() {
+        Board board = new Board
+                .Builder("제목 1", "자바국")
+                .content("내용 1")
+                .hit(1)
+                .builder();
+
+        assertThat(board.getTitle()).isEqualTo("제목 1" );
+        assertThat(board.getWriter()).isEqualTo("자바국");
+        assertThat(board.getContent()).isEqualTo("내용 1");
+        assertThat(board.getHit()).isEqualTo(1);
+
+    }
+
+}
