6a60bbdd513f2d53f183decea502c8c32492c50c
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-09-27 20:46:13 +0900

    test: 동작 검증, stubbing, Argument matchers, 호출 검증에 대한 테스트 코드 추가

---
+++ unit-testing/src/mock/ListTest.java
@@ -0,0 +1,73 @@
+package mock;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class ListTest {
+
+    @Test
+    void functionality_verification() {
+        List<String> mockedList = mock(List.class);
+
+        mockedList.add("one");
+        mockedList.add("two");
+
+        verify(mockedList).add("one");
+        verify(mockedList).add("two");
+        verify(mockedList, never()).clear();
+    }
+
+    @Test
+    void linkedList_stubbing() {
+        LinkedList<String> mockedList = mock(LinkedList.class);
+
+        when(mockedList.get(0)).thenReturn("frist");
+        when(mockedList.get(1)).thenThrow(new RuntimeException());
+
+        System.out.println(mockedList.get(0));
+        System.out.println(mockedList.get(999));
+
+        verify(mockedList).get(0);
+    }
+
+    @Test
+    void argument_matchers() {
+        LinkedList<String> mockedList = mock(LinkedList.class);
+
+        when(mockedList.get(anyInt())).thenReturn("element");
+
+        System.out.println(mockedList.get(999));
+
+        verify(mockedList).get(anyInt());
+    }
+
+    @Test
+    void times_verification() {
+        LinkedList<String> mockedList = mock(LinkedList.class);
+
+        mockedList.add("once");
+
+        mockedList.add("twice");
+        mockedList.add("twice");
+
+        mockedList.add("three times");
+        mockedList.add("three times");
+        mockedList.add("three times");
+
+        verify(mockedList).add("once");
+        verify(mockedList, times(1)).add("once");
+
+        verify(mockedList, times(2)).add("twice");
+        verify(mockedList, times(3)).add("three times");
+
+        verify(mockedList, never()).add("never happened");
+
+        verify(mockedList, atLeastOnce()).add("three times");
+        //verify(mockedList, atLeast(2)).add("five times");
+        //verify(mockedList, atMost(5)).add("three times");
+    }
+}
