7dbd07bacc0959e0d9255575e96ee9286f7245c0
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-10-06 19:55:16 +0900

    test: Selection, Quick, Counting Sort 관련 알고리즘 코드 추가

--- unit-testing/src/algorithm/SortAndSearch.java
+++ unit-testing/src/algorithm/SortAndSearch.java
@@ -19,21 +19,134 @@ public class SortAndSearch {
         // 최종 시간 복잡도 => O((N-1) * N / 2 * O(1)) = O(N^2)
         return arr;
     }
+
     public static int[] insertingSort(int[] arr) {
         int temp = 0;
 
         // O(N-1)
         for (int i = 1; i <= arr.length - 1; i++) {
             // O((N-1) * N / 2)
-           for( int j = i; j >= 1; j--) {
-               if(arr[j-1] > arr[j]) {
-                   temp = arr[j-1];
-                   arr[j-1] = arr[j];
-                   arr[j] = temp;
-               }
-           }
+            for (int j = i; j >= 1; j--) {
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
         }
         // 최종 시간 복잡도 => O((N-1) * N / 2 * O(1)) = O(N^2)
+        return arr;
+    }
+
+    public static int[] selectionSort(int[] arr) {
+        int minElement = 0; // 최소값
+        int minElementIndex = 0; // 최소값의 인덱스
+        int temp = 0; // 임시 저장 변수
+
+        // O(N) 만큼의 시간 소요
+        for (int currentIndex = 0; currentIndex < arr.length - 1; currentIndex++) {
+            minElement = arr[currentIndex];
+            minElementIndex = currentIndex;
+            temp = arr[currentIndex];
+
+            // O(N - 1) 만큼의 시간 소요
+            for (int i = currentIndex + 1; i < arr.length; i++) {
+                if (minElement > arr[i]) {
+                    minElement = arr[i];
+                    minElementIndex = i;
+                }
+            }
+
+            arr[currentIndex] = minElement;
+            arr[minElementIndex] = temp;
+        }
+
+        // 최종 시간 복잡도 = > O( N * (N - 1)) = O(N^2)
+        return arr;
+    }
+
+    public static int[] quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+
+            // pivot 선택에서 가장 큰 수를 선택 시 최악의 경우 O(N)
+            int partitionIdx = partition(arr, low, high);
+
+            // 최악 시간 복잡도 O(n)
+            quickSort(arr, low, partitionIdx - 1);
+            quickSort(arr, partitionIdx + 1, high);
+        }
+
+        // 최종 시간 복잡도
+        // Best, Average : O(n * log(n))
+        // Worst : O(N^2)
+        return arr;
+    }
+
+    private static int partition(int[] arr, int left, int right) {
+        int swapIdx = left - 1;
+        int pivotValue = arr[right];
+        int swapValue = 0;
+
+        for (int currentIndex = left; currentIndex <= right; currentIndex++) {
+            if (pivotValue > arr[currentIndex]) {
+                swapIdx++;
+                swapValue = arr[currentIndex];
+                arr[currentIndex] = arr[swapIdx];
+                arr[swapIdx] = swapValue;
+            }
+        }
+        swapIdx++;
+        swapValue = arr[swapIdx];
+        arr[swapIdx] = arr[right];
+        arr[right] = swapValue;
+
+        return swapIdx;
+    }
+
+    public static int[] countingSort(int[] arr) {
+        if (arr.length == 0) {
+            return arr;
+        }
+
+        int maxValue = 0;
+
+        // O(N) 만큼의 시간복잡도 소요
+        for (int value : arr) {
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+
+        int[] countingArray = new int[maxValue + 1];
+
+        // O(N) 만큼의 시간 복잡도 소요
+        for (int value : arr) {
+            countingArray[value]++;
+        }
+
+        int[] prefixSumArray = new int[maxValue + 1];
+        int sum = 0;
+
+        // O(N) 만큼의 시간 복잡도 소요
+        for (int i = 0; i <= prefixSumArray.length - 1; i++) {
+            sum += countingArray[i];
+            prefixSumArray[i] = sum - 1;
+        }
+
+        int[] sortedArray = new int[arr.length];
+
+        // O(N) 만큼의 시간 복잡도 소요
+        for (int value : arr) {
+            int idx = prefixSumArray[value];
+            prefixSumArray[value]--;
+            sortedArray[idx] = value;
+        }
+
+        // 최종 시간 복잡도 => O(N)!!
+        return sortedArray;
+    }
+
+    public static int[] radixSort(int[] arr) {
         return arr;
     }
 }
--- unit-testing/src/algorithm/SortAndSearchTest.java
+++ unit-testing/src/algorithm/SortAndSearchTest.java
@@ -23,12 +23,84 @@ class SortAndSearchTest {
         int[] sortedArr1 = {};
         assertThat(SortAndSearch.insertingSort(arr1)).isEqualTo(sortedArr1);
 
-        int[] arr2 = {6,4,1,8,9,2,7,5,3};
-        int[] sortedArr2 = {1,2,3,4,5,6,7,8,9};
+        int[] arr2 = {6, 4, 1, 8, 9, 2, 7, 5, 3};
+        int[] sortedArr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         assertThat(SortAndSearch.insertingSort(arr2)).isEqualTo(sortedArr2);
 
         int[] arr3 = {1};
         int[] sortedArr3 = {1};
         assertThat(SortAndSearch.insertingSort(arr3)).isEqualTo(sortedArr3);
+    }
+
+    @Test
+    void selectionSortTest() {
+        int[] arr1 = {};
+        int[] sortedArr1 = {};
+        assertThat(SortAndSearch.selectionSort(arr1)).isEqualTo(sortedArr1);
+
+        int[] arr2 = {6, 4, 1, 8, 9, 2, 7, 5, 3};
+        int[] sortedArr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertThat(SortAndSearch.selectionSort(arr2)).isEqualTo(sortedArr2);
+
+        int[] arr3 = {1};
+        int[] sortedArr3 = {1};
+        assertThat(SortAndSearch.selectionSort(arr3)).isEqualTo(sortedArr3);
+    }
+
+    @Test
+    void quickSortTest() {
+        int[] arr1 = {};
+        int[] sortedArr1 = {};
+        assertThat(SortAndSearch.quickSort(arr1, 0, arr1.length - 1)).isEqualTo(sortedArr1);
+
+        int[] arr2 = {6, 4, 1, 8, 9, 2, 7, 5, 3};
+        int[] sortedArr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertThat(SortAndSearch.quickSort(arr2, 0, arr2.length - 1)).isEqualTo(sortedArr2);
+
+        int[] arr3 = {1};
+        int[] sortedArr3 = {1};
+        assertThat(SortAndSearch.quickSort(arr3, 0, arr3.length - 1)).isEqualTo(sortedArr3);
+
+        int[] arr4 = {6, 4, 2, 10, 9, 1, 7, 11, 5, 3, 0, 8};
+        int[] sortedArr4 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        assertThat(SortAndSearch.quickSort(arr4, 0, arr4.length - 1)).isEqualTo(sortedArr4);
+    }
+
+    @Test
+    void countingSortTest() {
+        int[] arr1 = {};
+        int[] sortedArr1 = {};
+        assertThat(SortAndSearch.countingSort(arr1)).isEqualTo(sortedArr1);
+
+        int[] arr2 = {6, 4, 1, 8, 9, 2, 7, 5, 3};
+        int[] sortedArr2 = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertThat(SortAndSearch.countingSort(arr2)).isEqualTo(sortedArr2);
+
+        int[] arr3 = {1};
+        int[] sortedArr3 = {1};
+        assertThat(SortAndSearch.countingSort(arr3)).isEqualTo(sortedArr3);
+
+        int[] arr4 = {6, 4, 2, 10, 9, 1, 7, 11, 5, 3, 0, 8};
+        int[] sortedArr4 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        assertThat(SortAndSearch.countingSort(arr4)).isEqualTo(sortedArr4);
+    }
+
+    @Test
+    void radixSort() {
+        int[] arr = new int[5];
+        arr[0] = 52;
+        arr[1] = 31;
+        arr[2] = 24;
+        arr[3] = 45;
+        arr[4] = 13;
+
+        int[] sortedArr = new int[arr.length];
+        sortedArr[0] = 13;
+        sortedArr[1] = 24;
+        sortedArr[2] = 31;
+        sortedArr[3] = 45;
+        sortedArr[4] = 52;
+
+        assertThat(SortAndSearch.radixSort(arr)).isEqualTo(sortedArr);
     }
 }
