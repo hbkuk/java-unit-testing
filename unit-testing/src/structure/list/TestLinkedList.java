c86adfa04f7038c31d8dfd4f34d57e0e1d6fe8f9
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-11-05 21:44:38 +0900

    Linked List 중복 값 제거

--- unit-testing/src/structure/list/TestLinkedList.java
+++ unit-testing/src/structure/list/TestLinkedList.java
@@ -42,10 +42,32 @@ public class TestLinkedList {
 
         assertEquals(2, linkedList.size());
     }
+
+    @Test
+    void removeDuplicate() {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.append(33);
+        linkedList.append(34);
+        linkedList.append(35);
+        assertEquals(3, linkedList.size());
+
+        linkedList.append(33);
+        linkedList.append(34);
+        linkedList.append(35);
+        assertEquals(6, linkedList.size());
+
+        linkedList.append(33);
+        linkedList.append(34);
+        linkedList.append(35);
+        assertEquals(9, linkedList.size());
+
+        linkedList.removeDuplicates();
+        assertEquals(3, linkedList.size());
+    }
 }
 
-class LinkedList<T> {
-    private final Node<T> header;
+class LinkedList<E> {
+    private final Node<E> header;
     private int size;
 
     public LinkedList() {
@@ -56,27 +78,27 @@ class LinkedList<T> {
         return size;
     }
 
-    public void append(T data) {
-        Node<T> newNode = new Node(data);
+    public void append(E data) {
+        Node<E> newNode = new Node(data);
 
         if (isHeaderEmpty()) {
             header.next = newNode;
         } else {
-            Node<T> tempNode = header;
+            Node<E> tempNode = header;
             while (tempNode.next != null) {
                 tempNode = tempNode.next;
             }
             tempNode.next = newNode;
         }
-        size ++;
+        size++;
     }
 
     private boolean isHeaderEmpty() {
         return header.next == null;
     }
 
-    public void delete(T data) {
-        Node<T> tempNode = header;
+    public void delete(E data) {
+        Node<E> tempNode = header;
 
         while (tempNode.next != null) {
             if (tempNode.next.data == data) {
@@ -85,7 +107,23 @@ class LinkedList<T> {
                 tempNode = tempNode.next;
             }
         }
-        size --;
+        size--;
+    }
+
+    public void removeDuplicates() {
+        Node<E> pointer = header.next;
+        while (pointer != null) {
+            Node<E> runner = pointer;
+            while(runner.next != null) {
+                if (runner.next.data == pointer.data) {
+                    runner.next = runner.next.next;
+                    size --;
+                } else {
+                    runner = runner.next;
+                }
+            }
+            pointer = pointer.next;
+        }
     }
 }
 
